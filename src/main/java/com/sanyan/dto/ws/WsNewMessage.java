ca8a4b7260601c4344f397f207fe1531fa9a9e88
Aventador <ws419460186@gmail.com>
2026-04-08 13:04:40 +0800

    修复 WsNewMessage 缺少顶层 conversationId 导致客户端收不到回复

--- src/main/java/com/sanyan/dto/ws/WsNewMessage.java
+++ src/main/java/com/sanyan/dto/ws/WsNewMessage.java
@@ -6,9 +6,11 @@ import lombok.Data;
 @Data
 public class WsNewMessage {
     private final String type = "new_message";
+    private Long conversationId;
     private MessageData message;
 
     public WsNewMessage(MessageData message) {
         this.message = message;
+        this.conversationId = message.getConversationId();
     }
 }
